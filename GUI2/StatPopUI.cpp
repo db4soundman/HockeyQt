10b05c9f2b1d836842feb32523fdbcbd69be27f1
Doug Blase <db4soundman@gmail.com>
2017-08-02 20:27:47 -0400

    Make player stat bar UI have previews on top.

--- GUI2/StatPopUI.cpp
+++ GUI2/StatPopUI.cpp
@@ -6,9 +6,9 @@ StatPopUI::StatPopUI(HockeyGame *game, bool phome):
 {
     this->game=game;
     QVBoxLayout* myLayout = new QVBoxLayout();
-    myLayout->addLayout(getPopLayout());
     myLayout->addWidget(&seasonPreview);
     myLayout->addWidget(&gamePreview);
+    myLayout->addLayout(getPopLayout());
     setLayout(myLayout);
     connect(&playerSelector, SIGNAL(currentIndexChanged(int)), this, SLOT(updateGamePreview()));
     connect(&playerSelector, SIGNAL(currentIndexChanged(int)), this, SLOT(updateSeasonPreview()));
